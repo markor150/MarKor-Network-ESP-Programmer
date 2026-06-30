d16dbd5a4044482f8887dd5218fc03fae7203f45
Marek Kornaś <Tmarkor150@gmail.com>
2026-06-30 21:28:34 +0000

    Prepare Home Assistant add-on runtime

--- addon/network_esp_programmer/run.sh
+++ addon/network_esp_programmer/run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "======================================"
+bashio::log.info "===================================="
 bashio::log.info " MarKor Network ESP Programmer"
-bashio::log.info "======================================"
+bashio::log.info "===================================="
 
-echo
 python3 --version
 esptool.py version
 esphome version
-echo
-
-exec python3 /app/main.py
